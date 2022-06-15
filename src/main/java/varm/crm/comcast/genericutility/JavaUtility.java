f2639d5de658bf978bca4f98576180538508d3ea
HP <HP@LAPTOP-DCD51ORS>
2022-06-15 14:39:16 +0530

    System Date Method Updated

--- src/main/java/varm/crm/comcast/genericutility/JavaUtility.java
+++ src/main/java/varm/crm/comcast/genericutility/JavaUtility.java
@@ -12,6 +12,7 @@ public class JavaUtility
 		int ranDomNum=ranDom.nextInt(1000);
 		return ranDomNum;
 	}
+	//To get System Date....
 	public String getSystemDate()
 	{
 		String timeStamp = LocalDateTime.now().toString().replace(':', '-');
